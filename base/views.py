3703c8fb10912785c8987b0b54b014590cbaafb2
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2020-09-18 21:35:51 +0530

    created a template for homepage

--- base/views.py
+++ base/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<center><h2>Hi, I am Praful Sharma</h2>")
+    return render(request, 'base/home.html')
